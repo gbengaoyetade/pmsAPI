60d05396b02d2fbaa0b39d40a3523ad7cad19a77
gbengaoyetade <ioyetade@gmail.com>
2023-08-30 22:01:58 +0200

    add minor changes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import routes from "./routes";
 
 const app = express();
-// some comment
+// some comment and more comments
 app.use(express.urlencoded());
 app.use(express.json());
 app.get("/", (req, res) => {
